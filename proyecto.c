e9ed05f76d177481e8fffaafdb69f5296aa667f6
Jair Escamilla <hernandez_yo14@outlook.es>
2019-04-13 18:56:24 -0500

    Lista de doctores

--- proyecto.c
+++ proyecto.c
@@ -7,20 +7,67 @@ typedef struct defDoctores{
   char FullName[200];
   char Especialidad1[200];
   char Especialidad2[200];
-  int status;
+  int status;// 1-> Activo y 0-> Inactivo
   int telefonoUrgencias;
   char Direccion[200];
   int telefono;
   char ConsultorioAsignado[200];
   char diasConsulta[200];
-  char HorarioConsulta[200];
+  char HorarioConsulta[50];
+  struct defDoctores* sig;
 }Doctores;
 
 // Prototipos de las funciones
-
+void leerListaDoctores(Doctores**);
 // Función principal
 int main(int argc, char const *argv[]) {
-  printf("Comenzando a trabajar\n");
+  Doctores* ListaDoctores = NULL;
+  leerListaDoctores(&ListaDoctores);
   return 0;
 }
 // Desarrollando las funciones
+
+// Función para leer la lista de doctores
+void leerListaDoctores(Doctores** Lista){
+  char Name[200];
+  Doctores* Nuevo, *temp;
+  FILE* Archivo = fopen("doctores.txt", "rt");
+  if(Archivo == NULL){
+    Archivo = fopen("doctores.txt", "wt");
+    fprintf(Archivo, "DocPrueba\n");
+    fprintf(Archivo, "Neurólogo\n");
+    fprintf(Archivo, "Pediatría\n");
+    fprintf(Archivo, "1\n");
+    fprintf(Archivo, "5545455019\n");
+    fprintf(Archivo, "Paseo de la Reforma 880, Lomas de Santa Fe\n");
+    fprintf(Archivo, "5514129916\n");
+    fprintf(Archivo, "1A\n");
+    fprintf(Archivo, "Lunes y Martes\n");
+    fprintf(Archivo, "9:00 - 15:00\n");
+    fclose(Archivo);
+    Archivo = fopen("doctores.txt", "rt");
+  }
+  while (fscanf(Archivo, " %[^\n]", Name) == 1) {
+    Nuevo = (Doctores*)malloc(sizeof(Doctores));
+    strcpy(Nuevo->FullName, Name);
+    fscanf(Archivo, " %[^\n]", Nuevo->Especialidad1);
+    fscanf(Archivo, " %[^\n]", Nuevo->Especialidad2);
+    fscanf(Archivo, " %d", &Nuevo->status);
+    fscanf(Archivo, " %d", &Nuevo->telefonoUrgencias);
+    fscanf(Archivo, " %[^\n]", Nuevo->Direccion);
+    fscanf(Archivo, " %d", &Nuevo->telefono);
+    fscanf(Archivo, " %[^\n]", Nuevo->ConsultorioAsignado);
+    fscanf(Archivo, " %[^\n]", Nuevo->diasConsulta);
+    fscanf(Archivo, " %[^\n]", Nuevo->HorarioConsulta);
+    Nuevo->sig = NULL;
+    if(*Lista == NULL)
+      *Lista = Nuevo;
+    else{
+      temp = *Lista;
+      while(temp->sig != NULL)
+        temp = temp->sig;
+      temp->sig = Nuevo;
+    }
+  }
+  fclose(Archivo);
+}
