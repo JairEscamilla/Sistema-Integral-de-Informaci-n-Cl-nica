10ae8bf2ac6177b329db8f8a736b9c4d4e1d07e5
Jair Escamilla <hernandez_yo14@outlook.es>
2019-04-13 18:18:58 -0500

    Comentando

--- proyecto.c
+++ proyecto.c
@@ -1,8 +1,15 @@
+// Incluyendo las funciones
 #include <stdio.h>
 #include <string.h>
 #include <gtk/gtk.h>
+// Estructuras de datos
 
+
+// Prototipos de las funciones
+
+// Función principal
 int main(int argc, char const *argv[]) {
   printf("Comenzando a trabajar\n");
   return 0;
 }
+// Desarrollando las funciones
