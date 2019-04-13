a91d0da0ac023891fd28e2df170969e1c7a22e78
Jair Escamilla <hernandez_yo14@outlook.es>
2019-04-13 18:28:19 -0500

    Definiendo estructura de doctores

--- proyecto.c
+++ proyecto.c
@@ -3,7 +3,18 @@
 #include <string.h>
 #include <gtk/gtk.h>
 // Estructuras de datos
-
+typedef struct defDoctores{
+  char FullName[200];
+  char Especialidad1[200];
+  char Especialidad2[200];
+  int status;
+  int telefonoUrgencias;
+  char Direccion[200];
+  int telefono;
+  char ConsultorioAsignado[200];
+  char diasConsulta[200];
+  char HorarioConsulta[200];
+}Doctores;
 
 // Prototipos de las funciones
 
